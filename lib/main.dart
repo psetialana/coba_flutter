16b299583a13c261c418c1ed25889cc20451ae99
Pradana Setialana <pradana.setialana@gmail.com>
2021-09-14 20:08:22 +0700

    mengganti menjadi bahasa yang lebih baku

--- lib/main.dart
+++ lib/main.dart
@@ -94,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Anda sudah memencet tombol sebanyak:',
+              'Anda sudah menekan tombol sebanyak:',
             ),
             Text(
               '$_counter',
